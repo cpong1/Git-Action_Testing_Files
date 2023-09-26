22f43e14cad2d8e31d2f275b578cdc8f5e183d88
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2023-09-26 17:08:05 +0800

    Fix to chromedriver not opening issue

--- tests/unit/test_script.py
+++ tests/unit/test_script.py
@@ -3,6 +3,10 @@ import pytest
 from selenium import webdriver
 import time
 
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
