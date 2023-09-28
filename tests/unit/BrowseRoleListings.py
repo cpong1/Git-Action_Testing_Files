252106aac7133d0b702de02225eafebca1aa4e4c
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2023-09-28 11:24:47 +0800

    updates

--- tests/unit/BrowseRoleListings.py
+++ tests/unit/BrowseRoleListings.py
@@ -1,11 +1,5 @@
 from webdriver_imports import *
 
-# Define the URL of your GitHub Pages site
-github_pages_url = f"https://cpong1.github.io/Git-Action_Testing_Files/frontend/index.html?role=staff"
-
-# Navigate to your application URL
-driver.get(github_pages_url)
-
 listings = "listing_list"
 try:
     element = driver.find_element("id", listings)
