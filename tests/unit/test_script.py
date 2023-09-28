f63dad2162ddd7c3cf01750efbf0358d9a026026
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2023-09-28 16:31:50 +0800

    Update test_script.py

--- tests/unit/test_script.py
+++ tests/unit/test_script.py
@@ -2,7 +2,6 @@
 import pytest
 from selenium import webdriver
 import time
-import os
 
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -20,7 +19,7 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_option
 
 # get url
 # only works if we are using live server on our local machine in vs code
-driver.get("http://127.0.0.1:5500/templates/index.html")
+driver.get("https://git-action-testing-files.vercel.app/")
 
 
 def test_BrowseRoleListings():
@@ -36,6 +35,74 @@ def test_BrowseRoleListings():
         print("No listings found.")
 
     print("End of Test Case")
+
+def test_CreateRoleListings():
+    time.sleep(1)
+    expected_closing_dates = {}
+    # ensure that 'hr' is clicked
+    staff = driver.find_element(By.ID, "hr")
+    staff.click()
+    time.sleep(1)
+
+    # find create new role listing button and create a new role listing
+    element = driver.find_element(By.XPATH, "//button[@class='btn btn-dark']")
+    element.click()
+    time.sleep(1)
+    dropdown = Select(driver.find_element(By.ID, "roleTitle")) 
+    roleTitle = "Sales Representative"
+    dropdown.select_by_visible_text(roleTitle)
+
+    # Locate the input field that opens the date picker and enter in a date
+    year = "2023"
+    month = "09"
+    day = "27"
+    date = f"{year}-{month}-{day}"
+    date_input = driver.find_element(By.ID, "closingDate")
+    date_input.click()
+    date_input.send_keys(year)
+    date_input.send_keys(Keys.LEFT)
+    date_input.send_keys(month)
+    date_input.send_keys(Keys.LEFT)
+    date_input.send_keys(Keys.LEFT)
+    date_input.send_keys(day)
+    date_input.send_keys(Keys.RETURN)
+    time.sleep(1)
+
+    # save job listing
+    submit = driver.find_element(By.ID, "jobCreationButton")
+    submit.click()
+    time.sleep(3)
+
+    # close modal
+    close = driver.find_element(By.XPATH, "//button[@data-bs-dismiss='modal']")
+    close.click()
+
+    expected_closing_dates[roleTitle] = date # add to dictionary
+
+    # Locate all job title elements and closing date elements
+    job_title_elements = driver.find_elements(By.CSS_SELECTOR, ".card-title.m-2")
+    closing_date_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'text-center')]/h5[contains(text(), 'Closing Date:')]")
+
+    # Ensure there are job titles and closing dates
+    if job_title_elements and closing_date_elements:
+        # Get the latest job title and closing date
+        latest_job_title = job_title_elements[-1].text
+        latest_closing_date_element = closing_date_elements[-1]
+        latest_closing_date_text = latest_closing_date_element.text.split(":")[1].strip()
+
+        # Check if the latest closing date matches the expected date
+        if latest_job_title in expected_closing_dates:
+            expected_date = expected_closing_dates[latest_job_title]
+            if latest_closing_date_text == expected_date:
+                print(f"Latest Job Title: {latest_job_title}, Closing Date: {latest_closing_date_text} (Correct)")
+            else:
+                print(f"Latest Job Title: {latest_job_title}, Closing Date: {latest_closing_date_text} (Incorrect)")
+        else:
+            print(f"Latest Job Title: {latest_job_title} (No Expected Closing Date Found)")
+    else:
+        print("No job titles or closing dates found.")
+
+    print("Test case finished!")
 
 def test_ReadRoleListing_pos():
     time.sleep(1)
@@ -71,5 +138,6 @@ def test_ReadRoleListing_pos():
 
 
 test_BrowseRoleListings()
+test_CreateRoleListings()
 test_ReadRoleListing_pos()
 print("All test cases passed successfully!")
