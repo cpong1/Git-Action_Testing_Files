80e154dde370bfb636955ef7e76aa4e2a141e4a9
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2023-09-29 09:39:16 +0800

    Update test_script.py

--- tests/unit/test_script.py
+++ tests/unit/test_script.py
@@ -71,32 +71,32 @@ def ReadRoleListings():
     # Capture a screenshot and save it
     driver.save_screenshot('staff_browseTC_Screenshot.png')
 
-    # find create new role listing button
-    element = driver.find_element(By.XPATH, "//button[@class='btn btn-dark']")
-    actual_create_name = element.text
-    expected_create_name = "Create a Job Listing"
+#     # find create new role listing button
+#     element = driver.find_element(By.XPATH, "//button[@class='btn btn-dark']")
+#     actual_create_name = element.text
+#     expected_create_name = "Create a Job Listing"
 
-    # search for job listings 
-    listings = "listing_list"
-    try:
-        element = driver.find_element("id", listings)
-        print("Listings found.")
-        listingLoaded = True
-    except NoSuchElementException:
-        print("No listings found.")
-        listingLoaded = False
+#     # search for job listings 
+#     listings = "listing_list"
+#     try:
+#         element = driver.find_element("id", listings)
+#         print("Listings found.")
+#         listingLoaded = True
+#     except NoSuchElementException:
+#         print("No listings found.")
+#         listingLoaded = False
 
-    # search for edit button
-    edit = driver.find_element(By.XPATH, "//button[@class='btn btn-link']")
-    actual_edit_name = edit.text
-    expected_edit_name = "Edit"
+#     # search for edit button
+#     edit = driver.find_element(By.XPATH, "//button[@class='btn btn-link']")
+#     actual_edit_name = edit.text
+#     expected_edit_name = "Edit"
 
-    # check conditions
-    if (actual_create_name == expected_create_name) and listingLoaded and (actual_edit_name == expected_edit_name):
-        print("Test case passed!")
-    else:
-        print("Test case failed.")
+#     # check conditions
+#     if (actual_create_name == expected_create_name) and listingLoaded and (actual_edit_name == expected_edit_name):
+#         print("Test case passed!")
+#     else:
+#         print("Test case failed.")
 
-test_BrowseRoleListings()
-ReadRoleListings()
-print("All test cases passed successfully!")
+# test_BrowseRoleListings()
+# ReadRoleListings()
+# print("All test cases passed successfully!")
