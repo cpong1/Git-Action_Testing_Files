9b3b968f001839999a0489ada5ff3ee7e9912eeb
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2023-10-03 23:25:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,7 @@ def check_os():
     elif system == 'Darwin':
         return 'mysql+mysqlconnector://root:root@localhost:3306/SPM_KUIH'  # For macOS
     else:
-        # raise ValueError("Unsupported operating system")
-        # For unsupported operating systems, provide a default database URI or handle it as needed.
-        print("Unsupported operating system. Using default database URI.")
-        return 'mysql+mysqlconnector://root@localhost:3306/SPM_KUIH'
+        return 'mysql+mysqlconnector://root@localhost:3306/test'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = check_os()  # Set the URI based on the OS
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
