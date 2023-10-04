1a5fd450c8e1f16cb736e391b17070870e2a3996
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2023-10-04 16:48:40 +0800

    Update test.sql

--- tests/unit/test.sql
+++ tests/unit/test.sql
@@ -69,8 +69,8 @@ VALUES
 -- Insert data into the Job_Listing table
 INSERT INTO Job_Listing (Role_Name, publish_Date, Closing_date)
 VALUES
-    ('Software Developer', '2023-09-01', '2023-09-15'),
-    ('Data Analyst', '2023-09-02', '2023-09-16'),
+    ('Software Developer', '2023-09-01', '2023-11-15'),
+    ('Data Analyst', '2023-09-02', '2023-11-16'),
     ('Marketing Specialist', '2023-09-03', '2023-09-17'),
     ('Sales Representative', '2023-09-04', '2023-09-18'),
     ('Software Developer', '2023-09-05', '2023-09-19');
